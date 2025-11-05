67dfd7296a6aaa1513173b3cebf0c480b9d73d50
ruyibao96 <ruyi.bao@vorteklab.com>
2025-11-06 04:19:11 +0800

    test1

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
-
+all books from different shelves input:[(2,3),(1,2),(3,4)] expected output:[(1,2),(2,3),(3,4)]
+multiple books from the same shelf input:[(4,1),(4,3),(4,2)] expected output:[(4,1),(4,2),(4,3)]
+already in correct order input :[(4,1),(4,2),(4,3)] expected output:[(4,1),(4,2),(4,3)]
+books in reverse order: input:[(4,3),(4,2),(4,1)]expected output:[(4,1),(4,2),(4,3)]
 
