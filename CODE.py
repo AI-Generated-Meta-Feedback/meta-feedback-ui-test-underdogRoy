05f90eee22b9b0649bd0618d51cd1380527fb97c
ruyibao96 <ruyi.bao@vorteklab.com>
2025-11-06 04:18:08 +0800

    code 1

--- CODE.py
+++ CODE.py
@@ -1,1 +1,15 @@
-
+function sortReturnedBooks(books)
+  for i from 1 to len(books)-1:
+     cur=books[i]
+     j=i-1
+     while j>=0 and compare(cur,books[j]) is true:
+        books[j+1]=books[j]
+     books[j+1]=cur
+  return books
+function compare(a,b)
+ if a.shelfnumber<b.shelfnumber:
+     return true
+else if a.shelfnumber==b.shelfnumber and a.returnorder<b.returnorder:
+     return true
+else:
+    return false
